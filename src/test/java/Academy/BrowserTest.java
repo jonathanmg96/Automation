5a4c6cf2a35608889ac568a7bdebebb9a453db9a
jonathanmg96 <38457989+jonathanmg96@users.noreply.github.com>
2022-04-19 11:32:23 +0200

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,10 +19,10 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
+		driver.get("https://pruebajonathan.azurewebsites.net/Prueba/");
+		String text =driver.findElement(By.cssSelector("h2")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("Primer Cambio"));
 		driver.close();
 	
 		
